c2b38aa20923b5a3bb69974d9fecbb68216106a5
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2023-02-14 21:42:33 +0400

    Add get tour by id api

--- app.ts
+++ app.ts
@@ -26,13 +26,29 @@ app.get('/api/v1/tours', (req: Request, res: Response)=>{
   })
 });
 
+app.get('/api/v1/tours/:id', (req: Request, res: Response)=>{
+  const id = Number(req.params?.id);
+  const tour = tours.find((tour:any)=> tour.id === id
+  );
+
+  if(!tour) return res.status(404).json({
+    status: 'fail',
+    message: 'Invalid ID'
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: tour
+  })
+})
+
 app.post('/api/v1/tours', (req: Request, res: Response)=>{
   console.log(req.body);
   const newId = tours?.at(-1).id + 1;
   const newTour = Object.assign({id: newId}, req.body);
 
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), (err:any)=>{
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), ()=>{
     res.status(201).json({
       status: 'success',
       data: {
